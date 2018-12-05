a5d1bef5e2b53ebf9300e7938b618f58670b12f8
Peter Ruch <gfrogat@gmail.com>
2018-12-05 20:53:43 +0100

    Replace VERSION with _version.py

--- setup.py
+++ setup.py
@@ -19,58 +19,46 @@ class build_ui(setuptools.Command):
         subprocess.call(["npm", "install"], cwd="./confusionflow/ui")
         subprocess.call(["npm", "run", "build"], cwd="./confusionflow/ui")
         subprocess.call(
-            ["cp", "-r", "./confusionflow/ui/build", "./confusionflow/static"])
+            ["cp", "-r", "./confusionflow/ui/build", "./confusionflow/static"]
+        )
 
 
 class build_py(setuptools.command.build_py.build_py):
     def run(self):
         if not os.path.exists("./confusionflow/static"):
-            self.run_command('build_ui')
+            self.run_command("build_ui")
         setuptools.command.build_py.build_py.run(self)
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'confusionflow/VERSION')) as version_file:
-    version = version_file.read().strip()
+exec(open(os.path.join(here, "confusionflow", "_version.py")).read())
 
 
-readme = open('README.md').read()
+readme = open("README.md").read()
 
 
-requirements = [
-    "flask",
-    "gevent",
-    "pyyaml"
-]
+requirements = ["flask", "gevent", "pyyaml"]
 
-cmdclass = {
-    'build_py': build_py,
-    'build_ui': build_ui,
-}
+cmdclass = {"build_py": build_py, "build_ui": build_ui}
 
-entry_points = {
-    "console_scripts": [
-        "confusionflow=confusionflow:main"
-    ]
-}
+entry_points = {"console_scripts": ["confusionflow=confusionflow:main"]}
 
 setup(
     # Metadata
     name="confusionflow",
-    version=version,
+    version=__version__,
     author="ConfusionFlow Contributors",
     author_email="gfrogat@gmail.com",
     url="https://github.com/confusionflow/confusionflow",
     license="BSD-3-Clause",
     description="ConfusionFlow is a visualization tool that enables more nuanced monitoring of a neural network's training process.",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     project_urls={
-        'Documentation': 'https://docs.confusionflow.org',
-        'Source': 'https://github.com/confusionflow/confusionflow/',
-        'Tracker': 'https://github.com/confusionflow/confusionflow/issues',
+        "Documentation": "https://docs.confusionflow.org",
+        "Source": "https://github.com/confusionflow/confusionflow/",
+        "Tracker": "https://github.com/confusionflow/confusionflow/issues",
     },
-
     # Package info
     cmdclass=cmdclass,
     packages=find_packages(),
